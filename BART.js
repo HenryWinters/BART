1c1c579248130ee2e9998a97a1b9ff09f62583d8
Henry Winters <hlwinters17@gmail.com>
2021-07-23 20:38:18 -0700

    Added another API call to display upcoming departures based on the user's station selection

--- BART.js
+++ BART.js
@@ -1,6 +1,9 @@
-let stationSelectionContainer = document.querySelector('#station-selection');
+let stationSelector = document.querySelector('#station-selector');
+let stationSelection = "";
+let northboundDepartures = document.querySelector('#northbound-departures');
+let southboundDepartures = document.querySelector('#southbound-departures');
 
-function generateStations() {
+function generateStationOptions() {
     fetch('https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y')
     .then(response => response.json())
     .then(data => {
@@ -10,9 +13,48 @@ function generateStations() {
             let station = document.createElement('option');
             station.value = stationName; 
             station.textContent = stationName; 
-            stationSelectionContainer.appendChild(station);
+            stationSelector.appendChild(station);
          }
     }
 )};
 
-generateStations(); 
+generateStationOptions(); 
+
+stationSelector.addEventListener('change', (event) => {
+    northboundDepartures.textContent = "";
+    southboundDepartures.textContent = "";
+    let stationSelection = event.target.value; 
+    getStationDepartures(stationSelection);
+    console.log(stationSelection);
+});
+
+function getStationDepartures(stationToSearchFor) { 
+    fetch('https://api.bart.gov/api/etd.aspx?cmd=etd&orig=ALL&key=MW9S-E7SL-26DU-VV8V&json=y')
+    .then(response => response.json())
+    .then(data => {
+        let allStationsArr = data.root.station;
+        for (let i = 0; i < allStationsArr.length; i++) {
+            if (allStationsArr[i]['name'] == stationToSearchFor) {
+                let stationObj = allStationsArr[i];
+                let stationDeparturesArr = stationObj['etd'];
+                console.log(stationObj);
+                console.log(stationDeparturesArr);
+                for (let j = 0; j < stationDeparturesArr.length; j++) {
+                    let departureDestination = stationDeparturesArr[j]['destination'];
+                    let departureTime = stationDeparturesArr[j]['estimate'][0]['minutes'];
+                    let departureDirection = stationDeparturesArr[j]['estimate'][0]['direction'];
+                    let departureColor = stationDeparturesArr[j]['estimate'][0]['hexcolor'];
+                    let departurePlatform = stationDeparturesArr[j]['estimate'][0]['platform'];
+                    let departureLength = stationDeparturesArr[j]['estimate'][0]['length'];
+                    console.log(departureDirection);
+                    let departureListing = document.createElement('p'); 
+                    departureListing.textContent = `A ${departureLength}-car train heading towards ${departureDestination} is arriving in ${departureTime} minutes`;
+                    if (departureDirection === "North") {
+                        northboundDepartures.appendChild(departureListing);
+                    } else southboundDepartures.appendChild(departureListing);
+                }
+
+            } 
+        }
+    })
+};
